346dc1f5fb4c90ae0d56295ce0a953a48c325283
vivian347 <vkinyua001@gmail.com>
2022-10-10 22:44:38 +0300

    ubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - fn that sorts an array of ints in ascending order
+ * 	using bubble sort algos
+ *
+ * @array: array to be sorted
+ * @size: size of the array to be sorted
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t k, i;
+	int flag;
+	int temp;
+
+	for (k = 1; k < size; k++)
+	{
+		flag = 0;
+		for (i = 0; i <= size-k-1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				flag = 1;
+			}
+			print_array(array, size);
+		}
+		if (flag == 0)
+			break;
+	}
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+C - Sorting algorithms & Big O
+
+Sorting Algos
+--------------
+Selection sort
+bubble sort
+Merge Sort
+Quick Sort
+Inserion sort
+
+Big O-notation
+--------------
+O(1)
+O(n)
+O(n!)
+O(n^2)
+O(log(n))
+O(nlog(n))
+O(n+k)
+O(2^n)
