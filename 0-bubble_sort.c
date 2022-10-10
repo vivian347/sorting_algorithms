7e37d823e26a54e0a8dd672252821348958e45fe
vivian347 <vkinyua001@gmail.com>
2022-10-10 22:54:19 +0300

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,8 +25,8 @@ void bubble_sort(int *array, size_t size)
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
 				flag = 1;
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
 		if (flag == 0)
 			break;
