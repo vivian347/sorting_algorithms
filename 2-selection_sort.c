0c310ad58358753002cb6f19a2f28dde97888a58
vivian347 <vkinyua001@gmail.com>
2022-10-11 13:55:41 +0300

    selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array in ascending order using selection sort
+ * @array: pointer to the array to be sorted
+ * @size: size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, iMin;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		iMin = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[iMin])
+				iMin = j;
+		}
+		temp = array[i];
+		array[i] = array[iMin];
+		array[iMin] = temp;
+		print_array(array, size);
+	}
+}
